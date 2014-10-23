9b87d3de2cd5903caa8122a789754df51dff5714
samanthavandapuye <samantha.vandapuye@gmail.com>
2014-10-23 16:22:50 -0400

    todos

--- wp-content/themes/saintalban/page.php
+++ wp-content/themes/saintalban/page.php
@@ -12,14 +12,14 @@
  */
 
 get_header(); ?>
-<div class="border"></div>
+<div class="border">
 <div class="mainContainer">
 	<div class="headerInfo">
 		<div class="logoContainer">
 			<img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="Saint Alban">
 		</div>
 		<div class="addressContainer">
-			<p class="effra headerAddress"><?php the_field('address'); ?></p>
+			<p class="effra headerAddress"><a href="https://www.google.com/maps/place/710+King+St,+Charleston,+SC+29403/@32.798969,-79.946458,17z/data=!3m1!4b1!4m2!3m1!1s0x88fe7a5cbd6a1605:0x863feefdd436a06f" target="_blank"><?php the_field('address'); ?></a></p>
 		</div>
 		<div class="phoneContainer">
 			<p class="effra headerPhone"><?php the_field('phone_number'); ?></p>
@@ -27,8 +27,8 @@ get_header(); ?>
 	</div>
 
 	<div class="mapContainer">
-		<img class="desktopMap" src="<?php echo get_template_directory_uri(); ?>/images/desktop-map.jpg" alt="">
-		<img class="mobileMap"src="<?php echo get_template_directory_uri(); ?>/images/mobile-map.jpg" alt="">
+		<a href="https://www.google.com/maps/place/710+King+St,+Charleston,+SC+29403/@32.798969,-79.946458,17z/data=!3m1!4b1!4m2!3m1!1s0x88fe7a5cbd6a1605:0x863feefdd436a06f" target="_blank"><img class="desktopMap" src="<?php echo get_template_directory_uri(); ?>/images/desktop-map.jpg" alt=""></a>
+		<a href="https://www.google.com/maps/place/710+King+St,+Charleston,+SC+29403/@32.798969,-79.946458,17z/data=!3m1!4b1!4m2!3m1!1s0x88fe7a5cbd6a1605:0x863feefdd436a06f" target="_blank"><img class="mobileMap"src="<?php echo get_template_directory_uri(); ?>/images/mobile-map.jpg" alt=""></a>
 		<div class="parkingInfoContainer">
 			<div class="parkingInfoContent">
 				<span class="gothic parkingInfo"><em><?php the_field('parking_info'); ?></em></span>
@@ -41,6 +41,7 @@ get_header(); ?>
 		<h2 class="gothic footerContent"><?php the_field('footer_information'); ?></h2>
 	</div>
 
+</div>
 </div>
 
 
