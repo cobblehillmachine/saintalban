68b6f53f50db89273c5f0087e7d8b548e968fb59
maryhipp <maryhipp@gmail.com>
2015-06-05 15:18:40 -0400

    060515

--- wp-content/themes/saintalban/js/mytheme.js
+++ wp-content/themes/saintalban/js/mytheme.js
@@ -1,14 +1,16 @@
 $(window).load(function(){
 	if ($(window).width() > 1100) {
-		divEqualizer();
+// 		divEqualizer();
 	}
 	
 	homepageSlider();
+// 	tableCellWidth()
 })
 
 $(window).resize(function() {
+// 	tableCellWidth()
 	if ($(window).width() > 1100) {
-		divEqualizer();
+// 		divEqualizer();
 	}
 })
 
@@ -22,4 +24,10 @@ function homepageSlider() {
 function divEqualizer() {
 	var height = $('.left-cont').height();
 	$('.bottom-cont').css('height', height);
+}
+
+function mapMath() {
+	var height = $('.map-wrapper img').height();
+	var left = $('.left-cont').height() - $('.shop-hours.row').height() - $('.menu-cta-wrapper.row').height();
+	console.log(left)
 }
--- wp-content/themes/saintalban/page.php
+++ wp-content/themes/saintalban/page.php
@@ -51,12 +51,19 @@ get_header(); ?>
 					</div>
 					<?php $menu = get_field('menu_pdf');
 					if ($menu) { ?>
-						<div class="menu-cta row">
-							<a href="<?php the_field('menu_pdf'); ?>" target=_blank>
-								<span class="effra">Download</span>
-								<img src="<?php echo get_template_directory_uri(); ?>/images/our_menu.png" alt="">
-							</a>
+					<div class="menu-cta-wrapper row">
+						<div class="menu-cta">
+							<div class="top">
+								<div class="table-cell"><img src="<?php echo get_template_directory_uri(); ?>/images/our_menu.png" alt=""></div>
+								<div class="table-cell effra"><a target=_blank href="<?php the_field('drink_menu'); ?>">Drinks</a></div>
+							</div>
+							<div class="bottom effra">
+								<div class="table-cell"><a target=_blank href="<?php the_field('breakfast_menu'); ?>">Breakfast</a></div>
+								<div class="table-cell"><a target=_blank href="<?php the_field('lunch_menu'); ?>">Lunch</a></div>
+								<div class="table-cell"><a target=_blank href="<?php the_field('dinner_menu'); ?>">Dinner</a></div>
+							</div>
 						</div>
+					</div>
 					<?php } else {?>
 						<div class="menu-cta row empty">
 	
@@ -65,13 +72,16 @@ get_header(); ?>
 				</div>
 
 				<div class="map-wrapper">
-					<a href="https://www.google.com/maps/place/710+King+St,+Charleston,+SC+29403/@32.798969,-79.946458,17z/data=!3m1!4b1!4m2!3m1!1s0x88fe7a5cbd6a1605:0x863feefdd436a06f" target="_blank"><img class="map" src="<?php echo get_template_directory_uri(); ?>/images/mobile-map.jpg" alt=""></a>
-					<div class="parking-info-wrapper">
+					<a href="https://www.google.com/maps/place/710+King+St,+Charleston,+SC+29403/@32.798969,-79.946458,17z/data=!3m1!4b1!4m2!3m1!1s0x88fe7a5cbd6a1605:0x863feefdd436a06f" target="_blank" class="mobile"><img class="map" src="<?php echo get_template_directory_uri(); ?>/images/desktop-map.png" alt=""></a>
+					<a href="https://www.google.com/maps/place/710+King+St,+Charleston,+SC+29403/@32.798969,-79.946458,17z/data=!3m1!4b1!4m2!3m1!1s0x88fe7a5cbd6a1605:0x863feefdd436a06f" target="_blank"></a>
+					<!--
+<div class="parking-info-wrapper">
 						<div class="parking-info">
 							<span class="gothic parking"><em><?php the_field('parking_info'); ?></em></span>
 							<p class="effra"><?php the_field('address'); ?></p>
 						</div>
 					</div>
+-->
 				</div>
 			</div>
 		</div>
